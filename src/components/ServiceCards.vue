3c98cf3c0548aa2162ed031e4b7cb6580f1baf9b
sofMD <xsofiamaria.32@gmail.com>
2021-12-10 17:37:21 +0100

    serviceSection: card dinamiche

---
+++ src/components/ServiceCards.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+       <!-- image -->
+        <img :src="src" :alt="title">
+        <!-- title -->
+        <h3 class="mt-4" >{{title}}</h3>
+        <!-- description -->
+        <div class="description">{{description}}</div>
+        <!-- button -->
+        <button class="Buttons mt-3">{{button}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'ServiceCards',
+
+    props: {
+        src: String,
+        title: String,
+        description: String,
+        button: String
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+.serviceCard{
+
+            img {
+                width: 350px;
+            }
+
+            .description{
+                width: 275px;
+                margin: 10px auto;
+                font-size: 15px;
+            }
+        }
+</style>
--- src/components/ServiceSection.vue
+++ src/components/ServiceSection.vue
@@ -7,39 +7,12 @@
           <!-- card section -->
           <div class="d-flex justify-content-between pt-5">
               <!-- card -->
-              <div class="serviceCard">
-                  <!-- image -->
-                  <img src="../assets/avada-movers-serviceonephoto-final-400x255.jpg" alt="">
-                  <!-- title -->
-                  <h3 class="mt-4" >Two Man Teams</h3>
-                  <!-- description -->
-                  <div class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sed ipsa soluta dignissimos, et quam reprehenderit vero explicabo voluptatum provident!</div>
-                  <!-- button -->
-                  <button class="Buttons mt-3">Read More</button>
+              <div class="serviceCard" v-for="(el, index) in serviceList" :key="`service-${index}`">
 
+                  <ServiceCards :src="el.src" :title="el.title" :description="el.description" :button="el.button" />
+                  
               </div>
-              <!-- card 2-->
-              <div class="serviceCard">
-                  <!-- image -->
-                  <img src="../assets/avada-movers-servicetwophoto-final-400x255.jpg" alt="">
-                  <h3  class="mt-4">We Do All The Lifting</h3>
-                  <!-- description -->
-                  <div class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sed ipsa soluta dignissimos, et quam reprehenderit vero explicabo voluptatum provident!</div>
-                  <!-- button -->
-                  <button class="Buttons mt-3">Read More</button>
-
-              </div>
-              <!-- card 3-->
-              <div class="serviceCard">
-                  <!-- image -->
-                  <img src="../assets/avada-movers-servicethreephoto-final-400x255.jpg" alt="">
-                  <h3 class="mt-4">Coast To Coast</h3>
-                  <!-- description -->
-                  <div class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sed ipsa soluta dignissimos, et quam reprehenderit vero explicabo voluptatum provident!</div>
-                  <!-- button -->
-                  <button class="Buttons mt-3">Read More</button>
-
-              </div>
+              
           </div>
 
       </div>
@@ -47,8 +20,39 @@
 </template>
 
 <script>
+import ServiceCards from '@/components/ServiceCards.vue';
+
 export default {
     name: 'ServiceSection',
+
+    components: {
+        ServiceCards,
+    },
+
+    data(){
+        return {
+            serviceList: [
+                {
+                    src: require('../assets/avada-movers-serviceonephoto-final-400x255.jpg'),
+                    title: 'Two Man Teams',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sed ipsa soluta dignissimos, et quam reprehenderit vero explicabo voluptatum provident!',
+                    button: 'Read More',
+                },
+                {
+                    src: require('../assets/avada-movers-servicetwophoto-final-400x255.jpg'),
+                    title: 'We Do All The Lifting',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sed ipsa soluta dignissimos, et quam reprehenderit vero explicabo voluptatum provident!',
+                    button: 'Read More',
+                },
+                {
+                    src: require('../assets/avada-movers-servicethreephoto-final-400x255.jpg'),
+                    title: 'Coast To Coast',
+                    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sed ipsa soluta dignissimos, et quam reprehenderit vero explicabo voluptatum provident!',
+                    button: 'Read More',
+                }
+            ]
+        }
+    }
 }
 </script>
 
@@ -62,19 +66,6 @@ section{
         width: 65%;
         margin: 0 auto;
         text-align: center;
-
-        .serviceCard{
-
-            img {
-                width: 350px;
-            }
-
-            .description{
-                width: 275px;
-                margin: 10px auto;
-                font-size: 15px;
-            }
-        }
     }
 }
 
